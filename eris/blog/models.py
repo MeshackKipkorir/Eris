cba31fd02a207239154767bb0981eef33f3e864f
MeshackKipkorir <meshackkipkorir105@gmail.com>
2020-06-13 06:49:06 -0400

    added categories in the model

--- eris/blog/models.py
+++ eris/blog/models.py
@@ -1,31 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.utils import timezone
+from django.utils import timezone 
+from django.contrib.auth.models import User 
 from django.urls import reverse 
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager,self).get_queryset().filter(status='published')
+
+
+class Category(models.Model):
+    name = models.CharField(max_length = 20)
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft','Draft'),
         ('published','Published'),
     )
-    title = models.CharField(max_length = 250)
+    title = models.CharField(max_length=250)
     slug = models.SlugField(max_length = 250,unique_for_date='publish')
-    author = models.ForeignKey(User,on_delete = models.CASCADE,related_name = 'blog_posts')
-    body = models.TextField()
-    publish = models.DateTimeField(default = timezone.now)
-    created_on = models.DateTimeField(auto_now_add = True)
-    updated_on = models.DateTimeField(auto_now = True)
-    status = models.CharField(max_length = 10,choices = STATUS_CHOICES,default = 'draft')
-    published  = PublishedManager()
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
+    body =  models.TextField()
+    categories = models.ManyToManyField('Category',related_name='posts')
+    publish = models.DateTimeField(default=timezone.now)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
 
     class Meta:
         ordering = ('-publish',)
 
-
     def __str__(self):
         return self.title
-
     
