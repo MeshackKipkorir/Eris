fc931eb2348c7fb63431ad61a4bbc81de613770f
MeshackKipkorir <meshackkipkorir105@gmail.com>
2020-06-22 14:28:52 -0400

    added login and register to url path

--- eris/blog/urls.py
+++ eris/blog/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,name="post_detail"),
     path('<int:post_id>/share/',views.post_share,name='share_post'),
     path('feed/',LatestPostsFeed(), name = 'post_feed'),
+    path('register/',views.registerUser,name = 'register'),
+    path('login',views.login,name = 'login'),
 ]
