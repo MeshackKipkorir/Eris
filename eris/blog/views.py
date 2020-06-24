f5fa41f3f1adc2299ad1dad21449f9f11c72faa4
MeshackKipkorir <meshackkipkorir105@gmail.com>
2020-06-24 13:26:46 -0400

    fixed bug

--- eris/blog/views.py
+++ eris/blog/views.py
@@ -88,7 +88,7 @@ def user_login(request):
         if user is not None:
             if user.is_active:
                 login(request,user)
-                return redirect('/feed')
+                return redirect('/')
 
             else: 
                 return  HttpResponse('Account has been disabled')
