7d04e325cb47ef26516234668371ddf91f95becc
MeshackKipkorir <meshackkipkorir105@gmail.com>
2020-06-27 12:27:58 -0400

    edit form

--- eris/blog/forms.py
+++ eris/blog/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.models import User
+from .models import Profile
 
 class CommentForm(forms.Form):
     author = forms.CharField(max_length = 100, widget = forms.TextInput(attrs = {
@@ -41,3 +43,23 @@ class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget = forms.PasswordInput)
         
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label = 'password', widget = forms.PasswordInput)
+    password2 = forms.CharField(label = 'Repeat Password', widget = forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username','first_name','email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError(' passwords do not match')
+
+        return cd['password2']  
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('bio','photo')
