0d1f4ff8297b616f4b630d176530b26cf8993b07
MeshackKipkorir <meshackkipkorir105@gmail.com>
2020-06-27 12:17:11 -0400

    added media and media_root path

--- eris/eris/settings.py
+++ eris/eris/settings.py
@@ -130,6 +130,9 @@ EMAIL_HOST_PASSWORD = 'meshkor50'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+
 AUTHENTICATION_BACKENDS = [
 'django.contrib.auth.backends.ModelBackend',
 'blog.authentication.EmailAuthBackend', 
