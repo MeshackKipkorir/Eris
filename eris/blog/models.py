b639cabcd172e4a4231da4a2e18a6927c7dfe749
Meshack Kipkorir <meshackkipkorir105@gmail.com>
2020-07-01 21:57:32 +0300

    adds slug if user doesn't include slug

--- eris/blog/models.py
+++ eris/blog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse 
 from taggit.managers import TaggableManager
 from django.conf import settings
+from django.utils.text import slugify
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -37,6 +38,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
